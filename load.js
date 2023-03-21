fad5acf011e0a739f8f4786294b72121fe916104
Olivia Bizimungu <47276353+oliviabiz@users.noreply.github.com>
2023-03-21 16:41:55 -0400

    touch event for mobile instead of click

--- load.js
+++ load.js
@@ -54,33 +54,37 @@ else{
     console.log("desktop")
 }
 
-// Portrait mode only
-if(window.innerHeight > window.innerWidth){
-    const navbar = document.getElementById("navbar");
-    const container = document.querySelector(".container");
-    navbar.style.borderBottom = "none !important";
+window.onload = function() {
+    // Portrait mode only
+    if(window.innerHeight > window.innerWidth){
+        const navbar = document.getElementById("navbar");
+        const container = document.querySelector(".container");
 
+        const showNav = function() {
+            var content = this.children[1];
+            content.classList.toggle("visibleDrop");
+        
+            if (content.classList.contains("visibleDrop")) {
+                this.style.backgroundColor = "#f9f9f9"  
+                // container.style.setProperty("display", "none");
+                contentHeight = content.offsetHeight;
+                // console.log("shift by " + `${contentHeight}px`) 
+                navbar.style.setProperty("padding-bottom", `calc(${contentHeight}px)`, "important")
+                container.style.setProperty("margin-top", `calc(${contentHeight}px + 5vh)`, "important")    
+            
+            }
+            else{
+                // container.style.setProperty("display", "flex");
+                this.style.setProperty("background-color", "transparent", "important")
+                navbar.style.setProperty("padding-bottom", "0px", "important")
+                container.style.setProperty("margin-top", "10vh", "important")
+            }
+        }
 
-    window.onload = function() {
-        document.querySelectorAll(".dropdown").forEach(e => 
-            e.addEventListener("click", function() {
-                var content = this.children[1];
-                content.classList.toggle("visibleDrop");
-
-                if (content.classList.contains("visibleDrop")) {
-                    this.style.backgroundColor = "#f9f9f9"  
-                    contentHeight = content.offsetHeight;
-                    console.log("shift by " + `${contentHeight}px`) 
-                    navbar.style.setProperty("padding-bottom", `calc(${contentHeight}px)`, "important")
-                    container.style.setProperty("margin-top", `calc(${contentHeight}px + 5vh)`, "important")    
-                
-                }
-                else{
-                    this.style.setProperty("background-color", "white", "important")
-                    navbar.style.setProperty("padding-bottom", "0px", "important")
-                    container.style.setProperty("margin-top", "10vh", "important")
-                }
-
-            }));
-    };
+        document.querySelectorAll(".dropdown").forEach( function(e) {
+            e.addEventListener("click", showNav);
+            e.addEventListener("touchstart", showNav);
+        })
+    }
 }
+
