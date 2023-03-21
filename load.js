1f66cfa2b85656399360e53e134ee12f31e93639
Olivia Bizimungu <47276353+oliviabiz@users.noreply.github.com>
2023-03-21 15:54:09 -0400

    Fixed mobile navbar issue

--- load.js
+++ load.js
@@ -45,3 +45,42 @@ var fileList = Array();
             }
     });
 })();
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    // some code..
+    console.log("mobile")
+}
+else{
+    console.log("desktop")
+}
+
+// Portrait mode only
+if(window.innerHeight > window.innerWidth){
+    const navbar = document.getElementById("navbar");
+    const container = document.querySelector(".container");
+    navbar.style.borderBottom = "none !important";
+
+
+    window.onload = function() {
+        document.querySelectorAll(".dropdown").forEach(e => 
+            e.onclick = function () {
+                var content = this.children[1];
+                content.classList.toggle("visibleDrop");
+
+                if (content.classList.contains("visibleDrop")) {
+                    this.style.backgroundColor = "#f9f9f9"  
+                    contentHeight = content.offsetHeight;
+                    console.log("shift by " + `${contentHeight}px`) 
+                    navbar.style.setProperty("padding-bottom", `calc(${contentHeight}px)`, "important")
+                    container.style.setProperty("margin-top", `calc(${contentHeight}px + 5vh)`, "important")    
+                
+                }
+                else{
+                    this.style.setProperty("background-color", "white", "important")
+                    navbar.style.setProperty("padding-bottom", "0px", "important")
+                    container.style.setProperty("margin-top", "10vh", "important")
+                }
+
+            });
+    };
+}
